a7aec8d0f2fb548c2f9e0844867df05d47975933
falvizo <falvizo@bitmovil.mx>
2016-06-02 13:37:14 -0500

    Entrega Semana 2

--- busquedaISBN/ViewController.swift
+++ busquedaISBN/ViewController.swift
@@ -12,15 +12,65 @@ class ViewController: UIViewController{
     @IBOutlet weak var txtISBN: UITextField!
     @IBOutlet weak var txtResultado: UITextView!
     @IBOutlet weak var lblResultado: UILabel!
+    @IBOutlet weak var imgPortada: UIImageView!
     
     func sync(isbn : String) -> String{
-        var txt :String
+        //Guardamos la cadena URL de la consulta a JSON
         let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + isbn
+        //Casteamos la cadena de string al tipo NSURL para que pueda ser leida por NSData
         let url = NSURL(string: urls)
-        let datos:NSData? = NSData(contentsOfURL: url!)
+        //Obtenemos el contenido de la consulta JSON y lo almacenamos en datos (tipo NSData)
+        let datos = NSData(contentsOfURL: url!)
+        var autoresLibro = ""
+        var txt  = ""
         if datos != nil{
-            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
-            txt = texto! as String
+            //let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)   //Convertimos el contenido obtenido al tipo NSString
+            //txt = texto! as String //Lo convertimos a Sring.
+            
+
+            do {
+                
+                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                let dico1 = json as! NSDictionary
+                let dico2 = dico1 ["ISBN:"+isbn] as! NSDictionary
+                let titulo = dico2 ["title"] as! NSString as String //Título
+                let autoresArray = dico2["authors"] as! NSArray //Arreglo de autores
+                for autorLibro in autoresArray {
+                    let autorTemp = (autorLibro as! NSDictionary)["name"] as! NSString as String //Autor por autor
+                    autoresLibro += autorTemp
+                    autoresLibro += "; "
+                }
+                //Verificamos si la cadena JSON contiene la llave "cover"
+                var keys = dico2.allKeys
+                for _ in keys{
+                    if keys.popLast() as! String == "cover"{
+                        //si encontró cover
+                        let dico3 = dico2 ["cover"] as! NSDictionary //Portada
+                        var keysCover = dico3.allKeys //Obtenemos la cantidad de portadas
+                        //Verificamos si contiene una portada mediana
+                        for _ in keysCover{
+                            if keysCover.popLast() as! String == "medium"{
+                                //Obtenemos el link de la portada mediana
+                                let mediCover = dico3 ["medium"] as! NSString as String
+                                let imgURL = mediCover
+                                let urlImage = NSURL(string: imgURL)
+                                let datos = NSData(contentsOfURL: urlImage!)
+                                if datos != nil {
+                                    imgPortada.image=UIImage(data: datos!)
+                                }else{
+                                    imgPortada.image=nil
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+
+                txt = "Título: \(titulo) \nAutor(es): \(autoresLibro)"
+            }
+            catch _{
+                
+            }
             if txt == "{}"{
                 txt = "No se encontró el libro con el ISBN indicado"
             }
@@ -38,6 +88,7 @@ class ViewController: UIViewController{
     @IBAction func eliminarContenidoTxtField(sender: AnyObject) {
         txtResultado.text=""
         lblResultado.text=""
+        imgPortada.image=nil
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,11 +98,8 @@ class ViewController: UIViewController{
     
     @IBAction func esconderTeclado(sender: AnyObject) {
         self.resignFirstResponder()
-        //if txtISBN.text != nil {
             lblResultado.text="Resultado de la búsqueda"
             txtResultado.text = sync(txtISBN.text!)
-        //}
-        
     }
     
     
